0769d45594b408fb4458d44989bf4306a255ea8d
Aman <dv451197@gmail.com>
2023-11-09 00:32:31 +0530

    Added skeletons

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -12,27 +12,28 @@ import { formatCurrency } from './utils';
 import { unstable_noStore as noStore } from 'next/cache';
 
 export async function fetchRevenue() {
-  // Add noStore() here prevent the response from being cached.
-  // This is equivalent to in fetch(..., {cache: 'no-store'}).
   noStore()
+//   // Add noStore() here prevent the response from being cached.
+//   // This is equivalent to in fetch(..., {cache: 'no-store'}).
+//   noStore()
 
   try {
-    // Artificially delay a reponse for demo purposes.
-    // Don't do this in real life :)
+//     // Artificially delay a reponse for demo purposes.
+//     // Don't do this in real life :)
 
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+     // console.log('Fetching revenue data...');
+      await new Promise((resolve) => setTimeout(resolve, 3000));
 
-    const data = await sql<Revenue>`SELECT * FROM revenue`;
+     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
-    // console.log('Data fetch complete after 3 seconds.');
+ console.log('Data fetch complete after 3 seconds.');
 
-    return data.rows;
-  } catch (error) {
-    console.error('Database Error:', error);
-    throw new Error('Failed to fetch revenue data.');
-  }
-}
+     return data.rows;
+   } catch (error) {
+     console.error('Database Error:', error);
+     throw new Error('Failed to fetch revenue data.');
+   }
+ }
 
 export async function fetchLatestInvoices() {
   noStore()
